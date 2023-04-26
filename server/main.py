8116891658a8ec0f330333097f8ba4f7aea57d38
Filipe Cavalcanti <filipedoocv@gmail.com>
2023-04-26 18:28:16 -0300

    Updates to API

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 import ctypes
 import os
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
 
 libpath = os.path.normpath(
     os.path.join(
@@ -10,7 +11,7 @@ libpath = os.path.normpath(
 station_lib = ctypes.CDLL(libpath, mode=ctypes.RTLD_GLOBAL)
 station = station_lib.WrapperWeatherStation(ctypes.c_float(685))
 station_lib.WrapperUpdateStation(station)
-client_list = []
+clients = {}
 
 app = FastAPI()
 
@@ -20,28 +21,30 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.put("/clients/{client}")
-def register_client(client: int):
-    station_lib.WrapperClient.argtypes = [ctypes.c_char_p, ctypes.c_void_p]
-    client_name = f"Client_{client}"
-    cl = station_lib.WrapperClient(client_name.encode(), station)
-    client_list.append(cl)
-    return {"Message": "done"}
-
-
 @app.get("/station")
 def update_station():
     station_lib.WrapperUpdateStation(station)
     return {"Message": "done"}
 
+@app.post("/clients/{client}")
+def register_client(client: int):
+    if client in clients:
+        raise HTTPException(400, detail="Client already exists")
+    station_lib.WrapperClient.argtypes = [ctypes.c_char_p, ctypes.c_void_p]
+    print(f"Registering client: {client}")
+    client_name = f"Client_{client}"
+    cl = station_lib.WrapperClient(client_name.encode(), station)
+    clients[client] = cl
+    return {"Message": "done"}
+
 
 @app.get("/clients/{client}")
-def display(client: int):
+def display_client_temperature(client: int):
     station_lib.WrapperDisplayClient.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
     buffer = ctypes.create_string_buffer(30)
-    if client > len(client_list):
-        return {"Error": "Client not initialized"}
-    station_lib.WrapperDisplayClient(client_list[client - 1], buffer)
+    if client > len(clients):
+        raise HTTPException(status_code=404, detail="Client not initialized")
+    station_lib.WrapperDisplayClient(clients[client], buffer)
     data = buffer.value.decode()
     temperature, pressure = data.split(",")[0], data.split(",")[1]
-    return {"client": client, "temperature": temperature, "pressure": pressure}
+    return {"client": client, "temperature C": temperature, "pressure hPa": pressure}
